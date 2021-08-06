1408b923308fd3b44314c8eff28ac87e27a57156
Your Name <you@example.com>
2021-08-06 11:58:43 +0800

    第一次

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yjcsh/testmath
+
+go 1.16
---
+++ math.go
@@ -0,0 +1,8 @@
+package testmath
+
+import "fmt"
+
+func Add(n1, n2 int) int {
+	fmt.Println("github.com/yjcsh/testmath")
+	return n1 + n2
+}
